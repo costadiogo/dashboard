8dc33ac71fa4d9bea91202442072e2b7c0b9735c
costadiogo <costadiogo30@gmail.com>
2021-05-04 07:58:04 -0300

    Start project

---
+++ backend/src/main/java/com/dashboar/dcvends/DcvendasApplication.java
@@ -0,0 +1,13 @@
+package com.dashboar.dcvends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dashboar/dcvends/DcvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboar.dcvends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
