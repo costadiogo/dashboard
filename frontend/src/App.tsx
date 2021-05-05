ec346e84ccea93823360b3cb88894ad9c4daef80
costadiogo <costadiogo30@gmail.com>
2021-05-04 23:36:24 -0300

    Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
+import BarChart from "components/BarChart";
 import Navbar from "components/Navbar";
 import DataTable from "./components/DtaTable";
 import Footer from "./components/Footer";
+import PizzaChart from "./components/PizzaChart";
 function App() {
   return (
     <>
       <Navbar />
       <div className="container">
-        <h1 className="text-primary">Hello Word</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso </h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso </h5>
+            <PizzaChart />
+          </div>
+        </div>
+
+        <DataTable />
       </div>
-      <DataTable />
 
       <Footer />
     </>
